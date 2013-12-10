4ddb3768fdc618b6b5f14f4f0404ee68937d4139
Onikoroshi <petertcormack@gmail.com>
2013-12-09 21:18:26 -0800

    Initial Commit. Basic Rails Application.

---
+++ quiz/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quiz::Application.config.secret_key_base = '7dcb520fb251987bd1b14469620c308600bb3ac0ac9b7066645f5e0acf79da626f9ce0c5175130f891db497eed1a034b5a2887d5066c93483bcd3eee1f295ff3'
