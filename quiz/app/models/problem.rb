69e92418679554aa655b1cb23421439f17a8f87d
Onikoroshi <petertcormack@gmail.com>
2013-12-12 10:22:56 -0800

    new branch for addding Quiz (Exam) functionality.

--- quiz/app/models/problem.rb
+++ quiz/app/models/problem.rb
@@ -1,4 +1,5 @@
 class Problem < ActiveRecord::Base
+	has_and_belongs_to_many :aquizs
 	# each Math Problem needs to have both a Question and an Answer
 	validates :question, presence: true
 	validates :answer, presence: true
