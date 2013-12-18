ae1579e7fd7c73fa78431b9a40b921688837e3c8
Onikoroshi <petertcormack@gmail.com>
2013-12-17 19:58:31 -0800

    Starting to add some tests for functionality already in place.

--- test/models/problem_test.rb
+++ test/models/problem_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ProblemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save problem without a question" do
+    prob = Problem.new
+    prob.answer = "something"
+    assert !prob.save, "saved the problem without a question"
+  end
+
+  test "should not save problem without an answer" do
+    prob = Problem.new
+    prob.question = "something"
+    assert !prob.save, "saved the problem without an answer"
+  end
 end
