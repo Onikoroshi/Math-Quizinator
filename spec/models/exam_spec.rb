8e0d6ae214d2fb93daa874e0ab0e30ee405412d2
Onikoroshi <petertcormack@gmail.com>
2013-12-24 16:28:57 -0800

    updating.

--- spec/models/exam_spec.rb
+++ spec/models/exam_spec.rb
@@ -12,18 +12,7 @@ describe Exam do
     t = Exam.reflect_on_association(:problems)
     t.macro.should == :has_and_belongs_to_many
   end
-  it "sets problem list with given problem ids" do
-    exm = FactoryGirl.create(:exam)
-    prob_1 = FactoryGirl.create(:problem)
-    prob_2 = FactoryGirl.create(:problem)
-    prob_3 = FactoryGirl.create(:problem)
-    prob_4 = FactoryGirl.create(:problem)
-    probs = [prob_1, prob_2, prob_3, prob_4]
-
-    exm.problem_ids = [prob_1.id, prob_2.id, prob_3.id, prob_4.id]
-    exm.problems.should == probs
-  end
-  it "returns a result array and a score when scored" do
+  it "returns a proper Reporter object when scored" do
     exm = FactoryGirl.create(:exam)
 
     prob_1 = FactoryGirl.create(:problem)
@@ -32,16 +21,12 @@ describe Exam do
     prob_4 = FactoryGirl.create(:problem)
     exm.problem_ids = [prob_1.id, prob_2.id, prob_3.id, prob_4.id]
 
-    answers = [prob_1.answer, prob_2.answer+"b", prob_3.answer, prob_4.answer+"b"]
+    answers = {prob_1.id.to_s => prob_1.answer, prob_2.id.to_s => prob_2.answer+"b", prob_3.id.to_s => prob_3.answer, prob_4.id.to_s => prob_4.answer+"b"}
     correct_score = 2
-    correct_results = [
-        [prob_1.question, prob_1.answer, "", false],
-        [prob_2.question, prob_2.answer, answers[1], true],
-        [prob_3.question, prob_3.answer, "", false],
-        [prob_4.question, prob_4.answer, answers[3], true]
-      ]
-    score, results = exm.score(answers)
-    score.should == correct_score
-    results.should == correct_results
+    correct_total = 4
+
+    results = exm.evaluate(answers)
+    results.score.should == correct_score
+    results.total_possible.should == correct_total
   end
 end
--- spec/models/problem_spec.rb
+++ spec/models/problem_spec.rb
@@ -15,4 +15,21 @@ describe Problem do
     t = Problem.reflect_on_association(:exams)
     t.macro.should == :has_and_belongs_to_many
   end
+  it "should return a 1 when given the right answer" do
+    p = FactoryGirl.build(:problem)
+    correct_answer = p.answer
+    score = p.score(correct_answer)
+    score.should == 1
+  end
+  it "should return a 0 when given the wrong answer" do
+    p = FactoryGirl.build(:problem)
+    wrong_answer = p.answer + "b"
+    score = p.score(wrong_answer)
+    score.should == 0
+  end
+  it "should return a 1 when asked what it's worth" do
+    p = FactoryGirl.build(:problem)
+    points = p.worth
+    points.should == 1
+  end
 end
